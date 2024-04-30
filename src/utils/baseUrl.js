01672ea8c4f7c1795c559f99ae407d6bcff13d6b
Mehul-kapoor-21 <mehulkapoor308@gmail.com>
2024-05-01 00:07:33 +0530

    Update base url

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = "http://localhost:5000";
+const baseUrl = "https://expense-backend-rs4o.onrender.com";
 // const baseUrl = "https://expenses-tracker-api-v1.herokuapp.com";
 export default baseUrl;
